e2878cfc844dfe2828f12c80c0e7278073bf0098
nirmak22 <nirmak22@gmail.com>
2025-11-04 13:50:31 +0100

    google credentials in base64

--- main.tf
+++ main.tf
@@ -1,39 +1,15 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 6.0"
-    }
-  }
-
-  cloud {
-    organization = "composerv3-sentinel-policies"
-    workspaces {
-      name = "composerv3-sentinel"
-    }
-  }
-}
 provider "google" {
-  project = "kyndryl-sydney-sandbox"
-  region  = "us-central1"
+  credentials = base64decode(var.GOOGLE_CREDENTIALS_BASE64)
+  project     = var.GOOGLE_PROJECT
+  region      = var.GOOGLE_REGION
 }
 
-#Non-compliant: No encryption block
-resource "google_storage_bucket" "example" {
-  name     = "unencrypted-demo-bucket-${random_id.suffix.hex}"
-  location = "US"
+# Example resource: create a GCP storage bucket
+resource "google_storage_bucket" "example_bucket" {
+  name     = "terraform-cloud-example-bucket-${random_id.bucket_id.hex}"
+  location = var.GOOGLE_REGION
 }
 
-resource "random_id" "suffix" {
-  byte_length = 2
+resource "random_id" "bucket_id" {
+  byte_length = 4
 }
-
-# Compliant version (uncomment to test success)
-# resource "google_storage_bucket" "example" {
-#   name     = "encrypted-demo-bucket-${random_id.suffix.hex}"
-#   location = "US"
-#
-#   encryption {
-#     default_kms_key_name = "projects/your-gcp-project-id/locations/us-central1/keyRings/your-keyring/cryptoKeys/your-key"
-#   }
-# }
